0c1d906c5674d0a464f79a34a2d11242a61e7629
Maherov963 <126482312+Maherov963@users.noreply.github.com>
2024-03-15 17:00:52 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quran/models/quran.dart';
 import 'package:quran/screens/home_screen.dart';
-
+//hello
 late Quran quran;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
